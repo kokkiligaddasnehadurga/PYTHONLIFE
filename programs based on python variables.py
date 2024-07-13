8cfe7fa9eee32b78af030d65707543d81a46476a
KOKKILIGADDA SNEHA DURGA <144988647+kokkiligaddasnehadurga@users.noreply.github.com>
2024-07-13 10:04:00 +0530

    Create programs based on python variables.py

---
+++ programs based on python variables.py
@@ -0,0 +1,49 @@
+#varibale=value
+a=10
+print(a)
+
+#multiple values and multiple variables can be allowed
+a,b,c=1,3,4
+print(a,b,c)
+
+#multiple values can be stored in single variables
+c=1,2,3
+print(c)
+
+#multiple variables can allow single value with help of '=' operator
+c=v=x=22
+print(c,v,x)
+#we can also print like this
+c=v=x=22
+print(c)
+print(v)
+print(x)
+
+#variable allow
+abc=3
+print(abc)
+
+_abc=3
+print(_abc)
+
+abc35=100
+print(abc35)
+
+_abc45=200
+print(_abc45)
+
+#variable doesnot allow
+#@abc=abc35
+#print(@abc)
+
+#23abc=23
+#print(23abc)
+
+#we can allow upper case varibales or lower case variables for initializing values
+#for example
+python=1
+PYTHON=2
+print(python)
+print(PYTHON)
+#PYTHON IS A CASE SENSTIVE LANGUAGE
+
