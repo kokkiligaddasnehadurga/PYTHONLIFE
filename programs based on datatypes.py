e4c8e08c2641f778a8fee67939e9810fd6112ac4
KOKKILIGADDA SNEHA DURGA <144988647+kokkiligaddasnehadurga@users.noreply.github.com>
2024-07-13 10:19:08 +0530

    Create programs based on datatypes.py

---
+++ programs based on datatypes.py
@@ -0,0 +1,31 @@
+#programs based on datatypes in python
+#integer
+a=10
+b=-22
+print(type(a),type(b))
+
+#boolean
+a=True
+b=False
+print(type(a),type(b))
+
+#float
+c=4.44
+print(type(c))
+
+#complex
+v=2+5j
+print(type(v))
+
+#python allows conversion of one datatype to other data type
+#for example
+#int to float
+a=-22
+print(float(a))
+
+#float to int
+a=88.8
+print(int(a))
+
+#when the data is loss in output it is called explicit type conversion.
+#when the data is doesnot loss in output it is called implicit type function.
